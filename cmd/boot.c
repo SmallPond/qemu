3f6e3051023e1297142dcdc32fc76c8426f4bf0d
SmallPond <db_smpond@163.com>
2021-11-10 14:28:34 +0800

    add go_dtb cmd

--- cmd/boot.c
+++ cmd/boot.c
@@ -44,6 +44,34 @@ static int do_go(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
 	return rcode;
 }
 
+unsigned long do_go_exec_dtb(ulong (*entry)(ulong), int addr_dtb)
+{
+	return entry (addr_dtb);
+}
+
+static int do_go_dtb(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
+{
+	ulong	addr, rc, addr_dtb;
+	int     rcode = 0;
+
+	if (argc < 3)
+		return CMD_RET_USAGE;
+
+	addr = simple_strtoul(argv[1], NULL, 16);
+	addr_dtb = simple_strtoul(argv[2], NULL, 16);
+	printf ("## Starting application at 0x%08lX passing addr_dtb: 0x%08lX...\n", addr, addr_dtb);
+
+	/*
+	 * pass address parameter as argv[0] (aka command name),
+	 * and all remaining args
+	 */
+	rc = do_go_exec_dtb ((void *)addr, addr_dtb);
+	if (rc != 0) rcode = 1;
+
+	printf ("## Application terminated, rc = 0x%lX\n", rc);
+	return rcode;
+}
+
 /* -------------------------------------------------------------------- */
 
 U_BOOT_CMD(
@@ -52,7 +80,12 @@ U_BOOT_CMD(
 	"addr [arg ...]\n    - start application at address 'addr'\n"
 	"      passing 'arg' as arguments"
 );
-
+U_BOOT_CMD(
+	go_dtb, CONFIG_SYS_MAXARGS, 1,	do_go_dtb,
+	"set dtb to x0 and go to address 'addr' ",
+	"addr dtb addr \n    - start application at address 'addr'\n"
+	"      passing 'arg' as arguments"
+);
 #endif
 
 U_BOOT_CMD(
